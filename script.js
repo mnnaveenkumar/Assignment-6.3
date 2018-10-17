7329f7b7404dfe4c6293e92bb01d8721f98ee9d8
mnnaveenkumar <36623132+mnnaveenkumar@users.noreply.github.com>
2018-10-17 15:01:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var temperatureInCelsius = 40;
+
+// calculate Celsious
+var celsiusToFahrenheit;
+
+celsiusToFahrenheit = (temperatureInCelsius * 9) / 5 + 32;
+
+console.log("Temperature of " + temperatureInCelsius + " celsius, will be " + celsiusToFahrenheit + " fahrenheit");
+
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+var temperatureInFahrenheit = 104;
+
+// calculate fahrenheit
+var fahrenheitToCelsius;
+
+fahrenheitToCelsius = ((temperatureInFahrenheit - 32) * 5) / 9;
+
+console.log("Temperature of " + temperatureInFahrenheit + " fahrenheit, will be " + fahrenheitToCelsius + " celsius");
